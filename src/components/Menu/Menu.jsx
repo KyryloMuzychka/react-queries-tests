649edfd7d0874858001f70b7e2e893f1f339748e
KyryloMuzychka <kyryll2016@gmail.com>
2024-08-02 00:05:32 +0300

    added CreateAlbumTab

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,16 +1,10 @@
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
-import { Button } from "react-bootstrap";
-import { createAlbum } from "../../api";
 import AlbumTab from "../AlbumTab/AlbumTab";
 import AlbumByIdTab from "../AlbumByIdTab/AlbumByIdTab";
+import CreateAlbumTab from "../CreateAlbumTab/CreateAlbumTab";
 
 export default function Menu() {
-  async function addOne() {
-    const data = await createAlbum({ userId: 1, title: "qwerty" });
-    console.log(data);
-  }
-
   return (
     <Tabs
       defaultActiveKey="get-all-albums"
@@ -24,9 +18,8 @@ export default function Menu() {
       <Tab eventKey="get-album-by-id" title="Get Album By Id">        
         <AlbumByIdTab />        
       </Tab>
-      <Tab eventKey="create-album" title="Create Album">
-        <h1>Create Album</h1>
-        <Button onClick={addOne}>Create</Button>
+      <Tab eventKey="create-album" title="Create Album">        
+        <CreateAlbumTab />
       </Tab>
     </Tabs>
   );
