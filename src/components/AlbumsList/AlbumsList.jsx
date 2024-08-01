ce4663f0dd4fa712052ba24dd2e89a58a40833fb
KyryloMuzychka <kyryll2016@gmail.com>
2024-08-01 17:30:36 +0300

    set local styles for AlbumList

--- src/components/AlbumsList/AlbumsList.jsx
+++ src/components/AlbumsList/AlbumsList.jsx
@@ -1,14 +1,14 @@
-import "./AlbumList.module.css"
+import SingleAlbum from "../SingleAlbum/SingleAlbum";
+import AlbumListCSS from "./AlbumList.module.css";
 
 export default function AlbumList({ albums }) {
   return (
     <>
       <h1>Albums</h1>
-      <ul>
+      <ul className={AlbumListCSS.list}>
         {albums.map((album) => (
           <li key={album.id}>
-            <p>User Id: {album.userId}</p>
-            <p>Title: {album.title}</p>
+            <SingleAlbum album={album}/>            
             <hr />
           </li>
         ))}
