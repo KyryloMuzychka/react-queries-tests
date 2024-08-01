d662ed9b1d71f43a716c9f1bf8b3ba564f24b7f7
KyryloMuzychka <kyryll2016@gmail.com>
2024-08-01 17:31:02 +0300

    used AlbumByIdTab in Menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,15 +1,11 @@
 import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import { Button } from "react-bootstrap";
-import { fetchAlbumById, createAlbum } from "../../api";
+import { createAlbum } from "../../api";
 import AlbumTab from "../AlbumTab/AlbumTab";
+import AlbumByIdTab from "../AlbumByIdTab/AlbumByIdTab";
 
 export default function Menu() {
-  async function getOne() {
-    const data = await fetchAlbumById(2);
-    console.log(data);
-  }
-
   async function addOne() {
     const data = await createAlbum({ userId: 1, title: "qwerty" });
     console.log(data);
@@ -25,13 +21,12 @@ export default function Menu() {
       <Tab eventKey="get-all-albums" title="Get All Albums">
         <AlbumTab />
       </Tab>
-      <Tab eventKey="get-album-by-id" title="Get Album By Id">
-        <h1>Get Album By Id</h1>
-        <Button onClick={getOne}>Get One</Button>
+      <Tab eventKey="get-album-by-id" title="Get Album By Id">        
+        <AlbumByIdTab />        
       </Tab>
       <Tab eventKey="create-album" title="Create Album">
         <h1>Create Album</h1>
-        <Button onClick={addOne}>Create</Button>      
+        <Button onClick={addOne}>Create</Button>
       </Tab>
     </Tabs>
   );
