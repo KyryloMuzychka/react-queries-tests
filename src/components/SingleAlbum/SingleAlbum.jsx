270b1f6576dc951deb9d0e74833f815e71ff001f
KyryloMuzychka <kyryll2016@gmail.com>
2024-08-01 17:28:47 +0300

    added SingleAlbum

---
+++ src/components/SingleAlbum/SingleAlbum.jsx
@@ -0,0 +1,10 @@
+import SingleAlbumCSS from "./SingleAlbum.module.css";
+
+export default function SingleAlbum({ album }) {
+  return (
+    <>
+      <p className={SingleAlbumCSS.text}>User Id: {album.userId}</p>
+      <p className={SingleAlbumCSS.text}>Title: {album.title}</p>
+    </>
+  );
+}
