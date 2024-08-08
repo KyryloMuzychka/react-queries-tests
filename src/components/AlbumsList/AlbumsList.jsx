dd54d73b2bf5a0a087d5aa0af83dc0b947161af1
KyryloMuzychka <kyryll2016@gmail.com>
2024-08-08 22:46:55 +0300

    added test for AlbumList

--- src/components/AlbumsList/AlbumsList.jsx
+++ src/components/AlbumsList/AlbumsList.jsx
@@ -1,7 +1,10 @@
+import React from "react";
 import SingleAlbum from "../SingleAlbum/SingleAlbum";
 import AlbumListCSS from "./AlbumList.module.css";
 
 export default function AlbumList({ albums }) {
+  if (albums.length === 0) return <p>No albums available.</p>;
+
   return (
     <>
       <h1>Albums</h1>
